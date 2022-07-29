1aa52d874b700ae3ef7ef159b0e1fd990267fdc9
Marco Gorelli <marcogorelli@protonmail.com>
2022-07-29 16:59:02 +0200

    speed up

--- RPS and IR calculation.py
+++ RPS and IR calculation.py
@@ -18,44 +18,30 @@ asset_data = pd.read_csv("assets_m6.csv")
 #Read submission file (similar to the template provided by the M6 submission platform)
 submission_data = pd.read_csv("template.csv")
 
-hist_data = asset_data
+def ffill_missing_prices(hist_data):
+    pivoted = hist_data.pivot_table(index=['date'], columns=['symbol'], values=['price']).fillna(method='ffill', axis=0)
+    pivoted.columns = pivoted.columns.get_level_values(1)
+    pivoted = pivoted.reset_index()
+    hist_data = pivoted.melt(id_vars=['date'], value_name='price')
+    return hist_data
+
+hist_data = ffill_missing_prices(asset_data)
 submission = submission_data
+
+
 
 #Function for computing RPS
 def RPS_calculation(hist_data, submission, asset_no=100):
     
     if hist_data.shape[0]<=asset_no:
-        return np.nan    
-    
+        return np.nan
+
     asset_id = pd.unique(hist_data.symbol)
-    
-    for i in range(len(pd.unique(hist_data.date))):
-        if len(hist_data[hist_data.date == pd.unique(hist_data.date)[i]])<len(asset_id):
-            for asset in [x for x in asset_id if x not in hist_data[hist_data.date == pd.unique(hist_data.date)[i]].symbol.values]:
-                right_price = hist_data[hist_data.symbol==asset].sort_values(by='date')
-                right_price = right_price[right_price.date <= pd.unique(hist_data.date)[i]]
-                right_price = right_price.price.iloc[-1]
-                hist_data = hist_data.append({'date' : pd.unique(hist_data.date)[i],
-                                               'symbol' : asset,
-                                               'price' : right_price}, ignore_index=True)
 
     #Compute percentage returns
-    asset_id = sorted(asset_id) 
+    asset_id = sorted(asset_id)
 
-    #Compute percentage returns
-    returns = pd.DataFrame(columns = ["ID", "Return"])
-    
-    min_date = min(hist_data.date)
-    max_date = max(hist_data.date)
-    
-    for i in range(0,len(asset_id)):
-        temp = hist_data.loc[hist_data.symbol==asset_id[i]]
-        
-        open_price = float(temp.loc[temp.date==min_date].price)
-        close_price = float(temp.loc[temp.date==max_date].price)
-        
-        returns = returns.append({'ID': temp.symbol.iloc[0], 
-                                'Return': (close_price - open_price)/open_price}, ignore_index=True)
+    returns = hist_data.groupby('symbol')['price'].agg(('last', 'first')).pipe(lambda df: (df['last']-df['first'])/df['first']).rename_axis(index='ID').to_frame(name='Return').reset_index()
 
     #Define the relevant position of each asset
     ranking = pd.DataFrame(columns=["ID", "Position", "Return"])
@@ -66,47 +52,53 @@ def RPS_calculation(hist_data, submission, asset_no=100):
     #Handle Ties
     Series_per_position = pd.DataFrame(columns=["Position","Series", "Rank", "Rank1", "Rank2","Rank3", "Rank4", "Rank5"])
     Series_per_position.Position = list(pd.unique(ranking.Position.sort_values(ascending=True)))
-    temp = ranking.Position.value_counts()
-    temp = pd.DataFrame(zip(temp.index, temp), columns = ["Rank", "Occurencies"])
-    temp = temp.sort_values(by = ["Rank"],ascending=True)
-    Series_per_position.Series = list(temp.Occurencies)
-    Series_per_position
 
-    total_ranks = Series_per_position.Position.values[-1]
-    for i in range(0,Series_per_position.shape[0]):
-    
-        start_p = Series_per_position.Position[i]
-        end_p = Series_per_position.Position[i] + Series_per_position.Series[i]
-        temp = pd.DataFrame(columns = ["Position","Rank", "Rank1", "Rank2", "Rank3", "Rank4","Rank5"])
-        temp.Position = list(range(int(start_p),int(end_p)))
+    if len(Series_per_position) == len(asset_id):
+        # no ties, use fast code
+        returns['Rank'] = pd.qcut(returns['Return'], q=[0, .2, .4, .6, .8, 1]).cat.codes + 1
+        ranking =  pd.concat([ranking, returns['Rank'], pd.get_dummies(returns['Rank'], prefix='Rank').astype(float)], axis=1)
+    else:
+        temp = ranking.Position.value_counts()
+        temp = pd.DataFrame(zip(temp.index, temp), columns = ["Rank", "Occurencies"])
+        temp = temp.sort_values(by = ["Rank"],ascending=True)
+        Series_per_position.Series = list(temp.Occurencies)
+        Series_per_position
 
-        if(temp.loc[temp.Position.isin(list(range(1,int(0.2*total_ranks+1))))].empty==False):
-            temp.loc[temp.Position.isin(list(range(1,int(0.2*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(1,int(0.2*total_ranks+1))))].assign(Rank=1)
-            temp.loc[temp.Position.isin(list(range(1,int(0.2*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(1,int(0.2*total_ranks+1))))].assign(Rank1=1.0)
+        total_ranks = Series_per_position.Position.values[-1]
+        for i in range(0,Series_per_position.shape[0]):
 
-        elif(temp.loc[temp.Position.isin(list(range(int(0.2*total_ranks+1),int(0.4*total_ranks+1))))].empty==False):
-            temp.loc[temp.Position.isin(list(range(int(0.2*total_ranks+1),int(0.4*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.2*total_ranks+1),int(0.4*total_ranks+1))))].assign(Rank=2)
-            temp.loc[temp.Position.isin(list(range(int(0.2*total_ranks+1),int(0.4*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.2*total_ranks+1),int(0.4*total_ranks+1))))].assign(Rank2=1.0)
+            start_p = Series_per_position.Position[i]
+            end_p = Series_per_position.Position[i] + Series_per_position.Series[i]
+            temp = pd.DataFrame(columns = ["Position","Rank", "Rank1", "Rank2", "Rank3", "Rank4","Rank5"])
+            temp.Position = list(range(int(start_p),int(end_p)))
 
-        elif(temp.loc[temp.Position.isin(list(range(int(0.4*total_ranks+1),int(0.6*total_ranks+1))))].empty==False):
-            temp.loc[temp.Position.isin(list(range(int(0.4*total_ranks+1),int(0.6*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.4*total_ranks+1),int(0.6*total_ranks+1))))].assign(Rank=3)
-            temp.loc[temp.Position.isin(list(range(int(0.4*total_ranks+1),int(0.6*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.4*total_ranks+1),int(0.6*total_ranks+1))))].assign(Rank3=1.0)
+            if(temp.loc[temp.Position.isin(list(range(1,int(0.2*total_ranks+1))))].empty==False):
+                temp.loc[temp.Position.isin(list(range(1,int(0.2*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(1,int(0.2*total_ranks+1))))].assign(Rank=1)
+                temp.loc[temp.Position.isin(list(range(1,int(0.2*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(1,int(0.2*total_ranks+1))))].assign(Rank1=1.0)
 
-        elif(temp.loc[temp.Position.isin(list(range(int(0.6*total_ranks+1),int(0.8*total_ranks+1))))].empty==False):
-            temp.loc[temp.Position.isin(list(range(int(0.6*total_ranks+1),int(0.8*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.6*total_ranks+1),int(0.8*total_ranks+1))))].assign(Rank=4)
-            temp.loc[temp.Position.isin(list(range(int(0.6*total_ranks+1),int(0.8*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.6*total_ranks+1),int(0.8*total_ranks+1))))].assign(Rank4=1.0)
+            elif(temp.loc[temp.Position.isin(list(range(int(0.2*total_ranks+1),int(0.4*total_ranks+1))))].empty==False):
+                temp.loc[temp.Position.isin(list(range(int(0.2*total_ranks+1),int(0.4*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.2*total_ranks+1),int(0.4*total_ranks+1))))].assign(Rank=2)
+                temp.loc[temp.Position.isin(list(range(int(0.2*total_ranks+1),int(0.4*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.2*total_ranks+1),int(0.4*total_ranks+1))))].assign(Rank2=1.0)
 
-        elif(temp.loc[temp.Position.isin(list(range(int(0.8*total_ranks+1),int(total_ranks+1))))].empty==False):
-            temp.loc[temp.Position.isin(list(range(int(0.8*total_ranks+1),int(total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.8*total_ranks+1),int(total_ranks+1))))].assign(Rank=5)
-            temp.loc[temp.Position.isin(list(range(int(0.8*total_ranks+1),int(total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.8*total_ranks+1),int(total_ranks+1))))].assign(Rank5=1.0)
-        temp = temp.fillna(0)
-        Series_per_position.iloc[i,2:Series_per_position.shape[1]] = temp.mean(axis = 0).iloc[1:temp.shape[1]]
+            elif(temp.loc[temp.Position.isin(list(range(int(0.4*total_ranks+1),int(0.6*total_ranks+1))))].empty==False):
+                temp.loc[temp.Position.isin(list(range(int(0.4*total_ranks+1),int(0.6*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.4*total_ranks+1),int(0.6*total_ranks+1))))].assign(Rank=3)
+                temp.loc[temp.Position.isin(list(range(int(0.4*total_ranks+1),int(0.6*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.4*total_ranks+1),int(0.6*total_ranks+1))))].assign(Rank3=1.0)
 
-    Series_per_position = Series_per_position.drop('Series', axis = 1)
-    ranking = pd.merge(ranking,Series_per_position, on = "Position")
-    ranking = ranking[["ID", "Return", "Position", "Rank", "Rank1", "Rank2", "Rank3", "Rank4", "Rank5"]]
-    ranking = ranking.sort_values(["Position"])
-    
+            elif(temp.loc[temp.Position.isin(list(range(int(0.6*total_ranks+1),int(0.8*total_ranks+1))))].empty==False):
+                temp.loc[temp.Position.isin(list(range(int(0.6*total_ranks+1),int(0.8*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.6*total_ranks+1),int(0.8*total_ranks+1))))].assign(Rank=4)
+                temp.loc[temp.Position.isin(list(range(int(0.6*total_ranks+1),int(0.8*total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.6*total_ranks+1),int(0.8*total_ranks+1))))].assign(Rank4=1.0)
+
+            elif(temp.loc[temp.Position.isin(list(range(int(0.8*total_ranks+1),int(total_ranks+1))))].empty==False):
+                temp.loc[temp.Position.isin(list(range(int(0.8*total_ranks+1),int(total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.8*total_ranks+1),int(total_ranks+1))))].assign(Rank=5)
+                temp.loc[temp.Position.isin(list(range(int(0.8*total_ranks+1),int(total_ranks+1))))] = temp.loc[temp.Position.isin(list(range(int(0.8*total_ranks+1),int(total_ranks+1))))].assign(Rank5=1.0)
+            temp = temp.fillna(0)
+            Series_per_position.iloc[i,2:Series_per_position.shape[1]] = temp.mean(axis = 0).iloc[1:temp.shape[1]]
+
+        Series_per_position = Series_per_position.drop('Series', axis = 1)
+        ranking = pd.merge(ranking,Series_per_position, on = "Position")
+        ranking = ranking[["ID", "Return", "Position", "Rank", "Rank1", "Rank2", "Rank3", "Rank4", "Rank5"]]
+        ranking = ranking.sort_values(["Position"])
+
     #Evaluate submission
     rps_sub = []
     #for aid in list((pd.unique(ranking.ID))):
@@ -116,29 +108,17 @@ def RPS_calculation(hist_data, submission, asset_no=100):
         frc = np.cumsum(submission.loc[submission.ID==aid].iloc[:,1:6].values).tolist()
         rps_sub.append(np.mean([(a - b)**2 for a, b in zip(target, frc)]))
     submission["RPS"] = rps_sub
-    
+
     output = {'RPS' : np.mean(rps_sub),
               'details' : submission}
-    
+
     return(output)
-   
+
 #Function for computing IR
 def IR_calculation(hist_data, submission):
-    
+
     asset_id = pd.unique(hist_data.symbol)
-    
-    for i in range(len(pd.unique(hist_data.date))):
-        if len(hist_data[hist_data.date == pd.unique(hist_data.date)[i]])<len(asset_id):
-            for asset in [x for x in asset_id if x not in hist_data[hist_data.date == pd.unique(hist_data.date)[i]].symbol.values]:
-                right_price = hist_data[hist_data.symbol==asset].sort_values(by='date')
-                right_price = right_price[right_price.date <= pd.unique(hist_data.date)[i]]
-                right_price = right_price.price.iloc[-1]
-                hist_data = hist_data.append({'date' : pd.unique(hist_data.date)[i],
-                                               'symbol' : asset,
-                                               'price' : right_price}, ignore_index=True)
-
-
-    asset_id = sorted(asset_id) 
+    asset_id = sorted(asset_id)
 
     #Compute percentage returns
     returns = pd.DataFrame(columns = ["ID", "Return"])
@@ -146,26 +126,18 @@ def IR_calculation(hist_data, submission):
     #Investment weights
     weights = submission[["ID","Decision"]]
 
-    RET = pd.DataFrame()
-
-    for i in range(len(asset_id)):
-        temp = hist_data.loc[hist_data.symbol==asset_id[i]]
-        temp = temp.sort_values(by='date')
-        temp.reset_index(inplace=True, drop=True)
-        RET = RET.append(temp.price.pct_change()*weights.loc[weights.ID==asset_id[i]].Decision.values[0])
-
-    ret = np.log(1+RET.sum()[1:])
+    pivoted = hist_data.pivot_table(index=['date'], columns=['symbol'], values=['price']).pct_change(axis=0).iloc[1:]
+    pivoted.columns = pivoted.columns.get_level_values(1)
+    ret = np.log(1+(pivoted.reindex(columns=asset_id) * weights.set_index('ID').reindex(asset_id)['Decision'].to_numpy()).sum(axis=1)).reset_index(drop=True)
     sum_ret = sum(ret)
     sdp = stdev(ret)
     
     output = {'IR' : sum_ret/sdp,
               'details' : list(ret)}
-    
-    
     return output
-    
+
 #Run evaluation
-print('RPS: ', RPS_calculation(hist_data = asset_data , submission = submission_data)['RPS'])
+print('RPS: ', RPS_calculation(hist_data = hist_data , submission = submission_data)['RPS'])
 
 print('IR: ', IR_calculation(hist_data, submission)['IR'])
 
